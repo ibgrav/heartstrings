c074cecf09c58367a93e5c7ffb7d3f3ad6ea5500
Isaac Graves <igraves@hbs.edu>
2020-06-10 16:59:02 -0400

    fix callback

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = process.env.REDIRECT_URI + '/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
